ce9d80561e16a5bf894cbfb8bec9811094dd1e93
Akshay Kumar Sharma <akshay.sharma92@gmail.com>
2020-06-13 15:02:58 +0530

    Welcome component and routing

--- ShopingSite/src/app/app.module.ts
+++ ShopingSite/src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './Home/welcome/welcome.component';
+
+import { RouterModule } from '@angular/router'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
